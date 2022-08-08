1bcbed6860ce0f820cc7805a31c763ad2183dc8d
Pavel Máca <maca.pavel@gmail.com>
2022-08-08 14:57:56 +0200

    test

--- builds/ender3.js
+++ builds/ender3.js
@@ -16,7 +16,7 @@ module.exports = {
     configuration: {
         enable: [
             ["STRING_CONFIG_H_AUTHOR", "(Assassik, Ender-3 Pro)"],
-            ["MOTHERBOARD",  q\`BOARD_BTT_SKR_V1_3\`],
+            ["MOTHERBOARD",  q`BOARD_BTT_SKR_V1_3`],
             ["SERIAL_PORT",  -1],
             ["SERIAL_PORT_2",  0],
             ["X_DRIVER_TYPE",   q`TMC2209`],
@@ -51,7 +51,7 @@ module.exports = {
     configuration_adv: {
         enable: [
             ["E0_AUTO_FAN_PIN", q`FAN1_PIN`],
-            ["SDCARD_CONNECTION", "ONBOARD"],
+            ["SDCARD_CONNECTION", q`ONBOARD`],
             "MONITOR_DRIVER_STATUS",
             "TMC_DEBUG",
 
